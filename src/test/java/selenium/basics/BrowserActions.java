6e06d8e4abd64da5d8b7d175e329309a02496031
bharathautomation <soft.ittech20@gmail.com>
2025-04-14 09:03:23 +0530

    Selenium

---
+++ src/test/java/selenium/basics/BrowserActions.java
@@ -0,0 +1,35 @@
+package selenium.basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserActions {
+
+	public static void main(String[] args) {
+//		1. Launch the Browser window (Browser = Chrome)    
+		WebDriver driver = new ChromeDriver();		//EdgeDriver, FirefoxDriver
+		
+//		2. Minimize browser window     
+		
+//		3. Maximize to specific resolution(800X400)    
+//		4. Maximize the browser window   
+//		5. Delete all browser cookies     
+//		6. Enter URL and Launch the Application (https://www.google.co.in/)   
+//		7. Verify the application title (Google)  
+//		8. Navigate to Different application (https://www.selenium.dev/)
+//		9. Go back to previous application
+//		10. Move forward to next application 
+//		11. Refresh the application  
+//		12.collect the main window id     
+//		13.Launch new tab and Launch the application in new tab (https://in.search.yahoo.com/)   
+//		14. Switch back to the main window
+//		15.Launch new window and Launch the application in new window (https://parabank.parasoft.com/parabank/index.htm)  
+//		16.Switch back to main window  
+//		17.Print browser window URL  
+//		18. Get the Size of window     
+//		19. Close Current Window   
+//		20. Close all remaining windows
+
+	}
+
+}
